c2f70df20fa297c61fec2166c75b521cf4b98336
Yatin <yatinkathuria74@gmail.com>
2022-04-28 10:27:10 +0530

    lint fix

--- src/services/candidates.js
+++ src/services/candidates.js
@@ -1,63 +1,61 @@
-import apiClient from "../utils/api";
-import { showErrorNotification , showSuccessNotification} from '../utils/toast';
-
+import apiClient from '../utils/api';
+import { showErrorNotification, showSuccessNotification } from '../utils/toast';
 
 const fortmatResponse = (res) => {
   return JSON.stringify(res, null, 2);
 };
 
-export async function findCandidate({email,linkedInProfile}) {
+export async function findCandidate({ email, linkedInProfile }) {
   if (email || linkedInProfile) {
     let url = `candidates/getCandidateByAny?email=${email}`;
-    if(linkedInProfile){
+    if (linkedInProfile) {
       url = `candidates/getCandidateByAny?linkedInProfile=${linkedInProfile}`;
-    } 
+    }
     try {
       const res = await apiClient.get(url);
-      
-      if(res?.data?.code !== 200){
-        throw new Error(res.data.message || "user not found");
+
+      if (res?.data?.code !== 200) {
+        throw new Error(res.data.message || 'user not found');
       }
       const result = res?.data?.candidate;
-      console.log("getRecruter",fortmatResponse(result))
+      console.log('getRecruter', fortmatResponse(result));
       return result;
     } catch (err) {
-      console.log("getRecruter",fortmatResponse(err.response?.data || err))
+      console.log('getRecruter', fortmatResponse(err.response?.data || err));
 
       showErrorNotification(err.message);
-      
+
       return err;
     }
   }
 }
 
 export async function createCandidates(postData) {
-    try {
-        const res = await apiClient.post("/candidates/addcandidate?", postData, {
-          headers: {
-            "x-access-token": "token-value",
-          },
-        });
+  try {
+    const res = await apiClient.post('/candidates/addcandidate?', postData, {
+      headers: {
+        'x-access-token': 'token-value',
+      },
+    });
 
-        if(res?.data?.code !== 200){
-          throw new Error(res.data.message || "User info already added!!");
-        }
-  
-        const result = {
-          status: res.status + "-" + res.statusText,
-          headers: res.headers,
-          data: res.data,
-        };
+    if (res?.data?.code !== 200) {
+      throw new Error(res.data.message || 'User info already added!!');
+    }
 
-        console.log("createCandidates",fortmatResponse(result))
-        showSuccessNotification('Data Added Successfully!! Add more people');
-        return result;
-      } catch (err) {
-        console.log(fortmatResponse(err.response?.data || err));
+    const result = {
+      status: res.status + '-' + res.statusText,
+      headers: res.headers,
+      data: res.data,
+    };
 
-        showErrorNotification(err.message);
+    console.log('createCandidates', fortmatResponse(result));
+    showSuccessNotification('Data Added Successfully!! Add more people');
+    return result;
+  } catch (err) {
+    console.log(fortmatResponse(err.response?.data || err));
 
-        return err;
-      }
+    showErrorNotification(err.message);
+
+    return err;
   }
-
+}
