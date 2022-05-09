e48c2388cc790c37122a887005f5606d2ee828d6
Raqesh8181 <raqeshsuthar@gmail.com>
2022-05-09 17:48:56 +0530

    Fix styling

--- src/components/SkillView.js
+++ src/components/SkillView.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const SkillView = ({ data }) => {
   const [openAccodingId, setOpenAccodingId] = useState(-1);
@@ -32,7 +32,7 @@ const SkillView = ({ data }) => {
       stars.push(
         <i
           className={`fas fa-star mr-1 ${
-            yellow >= i ? 'text-yellow-400' : 'text-gray-400'
+            yellow >= i ? "text-yellow-400" : "text-gray-400"
           }`}
           key={i}
         />
@@ -43,34 +43,34 @@ const SkillView = ({ data }) => {
   };
 
   const getRatingBar = (rating) => {
-    console.log('rating:', rating);
+    console.log("rating:", rating);
     if (rating <= 100 && rating > 91)
       return (
         <span
-          className={'w-12 h-3 bg-purple-600 inline-block rounded-lg mr-4'}
+          className={"w-12 h-3 bg-purple-600 inline-block rounded-lg mr-4"}
         ></span>
       );
     else if (rating <= 90 && rating > 71)
       return (
         <span
-          className={'w-12 h-3 bg-green-600 inline-block rounded-lg mr-4'}
+          className={"w-12 h-3 bg-green-600 inline-block rounded-lg mr-4"}
         ></span>
       );
     else
       return (
         <span
-          className={'w-12 h-3 bg-orange inline-block rounded-lg mr-4'}
+          className={"w-12 h-3 bg-orange inline-block rounded-lg mr-4"}
         ></span>
       );
   };
 
   return (
     <div>
-      <div className={'font-medium text-gray-600'}>
-        <div className={'grid grid-cols-12 my-4'}>
-          <span className={'col-span-4'}>Skill</span>
-          <span className={'col-span-4'}>Vetting results</span>
-          <span className={'col-span-4'}>Years of experience</span>
+      <div className={"font-medium text-gray-600"}>
+        <div className={"grid grid-cols-12 my-4"}>
+          <span className={"col-span-4"}>Skill</span>
+          <span className={"col-span-4"}>Vetting results</span>
+          <span className={"col-span-4"}>Years of experience</span>
         </div>
         <div className="bg-white mx-auto">
           <div className="shadow-box p-0">
@@ -88,8 +88,8 @@ const SkillView = ({ data }) => {
                         className="w-full px-0 py-6 text-left"
                         onClick={(e) => handleClick(e, index)}
                       >
-                        <div className={'grid grid-cols-12 mt-4'}>
-                          <span className={'col-span-4'}>
+                        <div className={"grid grid-cols-12 mt-4"}>
+                          <span className={"col-span-4"}>
                             {openAccodingId === index ? (
                               <i className="fas fa-chevron-up my-1 mr-2 text-gray-400" />
                             ) : (
@@ -99,82 +99,82 @@ const SkillView = ({ data }) => {
                               {info.topic}
                             </span>
                           </span>
-                          <span className={'col-span-4'}>
+                          <span className={"col-span-4"}>
                             {renderStars(9 * 10)}
                           </span>
-                          <span className={'col-span-4'}>4 Years</span>
+                          <span className={"col-span-4"}>4 Years</span>
                         </div>
                       </button>
 
                       <div
                         className={`pl-4 border-b border-gray-200 relative overflow-auto transition-all duration-700 ${
-                          openAccodingId === index ? 'h-auto' : 'h-0'
+                          openAccodingId === index ? "h-auto" : "h-0"
                         }`}
                       >
                         <div className="grid grid-cols-12 ">
-                          <div className={'col-span-8'}>
-                            <div className={' grid grid-cols-2 '}>
+                          <div className={"col-span-8"}>
+                            <div className={" grid grid-cols-2 "}>
                               {info.concepts.map((subtopic) => (
                                 <React.Fragment key={subtopic.name}>
-                                  <span className={'col-span-1'}>
+                                  <span className={"col-span-1"}>
                                     {subtopic.name}
                                   </span>
-                                  <span className={'col-span-1'}>
+                                  <span className={"col-span-1"}>
                                     {getRatingBar(subtopic.rating * 10)}
                                     {subtopic.rating * 10}
-                                    {'%'}
+                                    {"%"}
                                   </span>
                                 </React.Fragment>
                               ))}
                             </div>
                           </div>
 
-                          <div className={'col-span-4'}>
-                            <div className={'flex flex-col pb-3'}>
+                          <div className={"col-span-4"}>
+                            <div className={"flex flex-col pb-3"}>
                               <span
-                                className={'font-medium text-gray-800 text-xs'}
+                                className={"font-medium text-gray-800 text-xs"}
                               >
-                                {' '}
+                                {" "}
                                 How they compare with other developers in
                                 Thinkify
                               </span>
-                              <div className={'flex flex-row mt-1'}>
+                              <div className={"flex flex-row mt-1"}>
                                 <span
                                   className={
-                                    'w-3 h-3 bg-purple-600 inline-block mr-4'
+                                    "w-3 h-3 bg-purple-600 inline-block mr-4"
                                   }
                                 ></span>
                                 <span
                                   className={
-                                    'font-medium text-gray-800 text-xs'
+                                    "font-medium text-gray-800 text-xs"
                                   }
                                 >
                                   Top 10%
                                 </span>
                               </div>
-                              <div className={'flex flex-row mt-1'}>
+                              <div className={"flex flex-row mt-1"}>
                                 <span
                                   className={
-                                    'w-3 h-3 bg-green-600 inline-block mr-4'
+                                    "w-3 h-3 bg-green-600 inline-block mr-4"
                                   }
                                 ></span>
                                 <span
                                   className={
-                                    'font-medium text-gray-800 text-xs'
+                                    "font-medium text-gray-800 text-xs"
                                   }
                                 >
                                   Top 20%
                                 </span>
                               </div>
-                              <div className={'flex flex-row mt-1'}>
+                              <div className={"flex flex-row mt-1"}>
                                 <span
                                   className={
-                                    'w-3 h-3 bg-orange inline-block mr-4'
+                                    "w-3 h-3 bg-orange inline-block mr-4"
                                   }
                                 ></span>
                                 <span
                                   className={
-                                    'font-medium text-gray-800 text-xs'
+                                    "font-medium text-gray-800 text-xs"
                                   }
                                 >
                                   Top 60%
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { withRouter } from 'react-router';
-import { findCandidate } from '../services/candidates';
-import { useHistory, useLocation } from 'react-router';
-import AddCandidateBanner from '../components/AddCandidateBanner';
-import SkillView from '../components/SkillView';
-import whatIsTheQueryKey from '../utils/findapi.utlis';
-import data from '../utils/demo';
-import { Avatar } from '@mui/material';
-import { stringAvatar, stringToColor } from '../utils/mui.utils';
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router";
+import { findCandidate } from "../services/candidates";
+import { useHistory, useLocation } from "react-router";
+import AddCandidateBanner from "../components/AddCandidateBanner";
+import SkillView from "../components/SkillView";
+import whatIsTheQueryKey from "../utils/findapi.utlis";
+import data from "../utils/demo";
+import { Avatar } from "@mui/material";
+import { stringAvatar, stringToColor } from "../utils/mui.utils";
 
 const images = {
-  flipkart: 'flipkart.png',
-  amazon: 'amazon.png',
+  flipkart: "flipkart.png",
+  amazon: "amazon.png",
 };
 
+const skills = [
+  "HTML",
+  "CSS",
+  "JS",
+  "Bootstrap",
+  "jQuery",
+  "ReactJS",
+  "Redux",
+  "NextJS",
+];
+
 const Find = () => {
-  const [candidate, setCandidate] = useState('');
-  const [candidateSearch, setCandidateSearch] = useState('');
+  const [candidate, setCandidate] = useState("");
+  const [candidateSearch, setCandidateSearch] = useState("");
   const [candidateEmptyError, setCandidateEmptyError] = useState(false);
 
   const location = useLocation();
   const history = useHistory();
 
   const params = new URLSearchParams(location.search);
-  const hideFindBox = params.get('hf');
+  const hideFindBox = params.get("hf");
 
   const goToAddIngo = () => {
     debugger;
@@ -38,7 +49,7 @@ const Find = () => {
       const res = await findCandidate({ [profile]: value });
       if (res.message) {
         setCandidateEmptyError(true);
-        setCandidate('');
+        setCandidate("");
       } else {
         setCandidate(res);
         setCandidateEmptyError(false);
@@ -50,7 +61,7 @@ const Find = () => {
 
   useEffect(() => {
     const params = new URLSearchParams(location.search);
-    const slug = params.get('find');
+    const slug = params.get("find");
     if (slug) {
       findAndSetCandidate(slug);
     }
@@ -80,7 +91,7 @@ const Find = () => {
             >
               Search Candidate
             </p>
-            <div className={'mt-8'}>
+            <div className={"mt-8"}>
               <label
                 id="data"
                 className="text-sm font-medium leading-none text-gray-800"
@@ -109,54 +120,59 @@ const Find = () => {
       {candidate?.email && (
         <div
           className={`bg-white overflow-auto h-11/12 p-3 shadow-sm rounded-sm ${
-            candidate?.email && hideFindBox ? 'col-span-6' : 'col-span-4'
+            candidate?.email && hideFindBox ? "col-span-6" : "col-span-4"
           }`}
         >
           <div>
-            <div className="flex items-center space-x-2 px-4 font-semibold text-gray-900 leading-8">
-              <span clas="text-green-500">
-                <Avatar
-                  {...stringAvatar(candidate.name || '')}
-                  sx={{
-                    width: 30,
-                    height: 30,
-                    bgcolor: stringToColor(candidate.name || ''),
-                    fontSize: 16,
-                  }}
-                />
-              </span>
-              <span className="tracking-wide capitalize">
-                {candidate?.name}
-              </span>
-              <span onClick={handleEdit} className="cursor-pointer">
-                <i className="fas fa-edit" />
-              </span>
-              {candidate.linkedInProfile && (
-                <a
-                  href={`https://www.linkedin.com/in/${candidate.linkedInProfile}/`}
-                >
-                  <img
-                    className="h-5 w-5 inline-block"
-                    src="/images/linkendin.png"
-                    alt=""
+            <div className="flex justify-between">
+              <div className="flex items-center space-x-2 px-4 font-semibold text-gray-900 leading-8">
+                <span clas="text-green-500">
+                  <Avatar
+                    {...stringAvatar(candidate.name || "")}
+                    sx={{
+                      width: 30,
+                      height: 30,
+                      bgcolor: stringToColor(candidate.name || ""),
+                      fontSize: 16,
+                    }}
                   />
-                </a>
-              )}
-              {candidate.gitHub && (
-                <a href={`https://github.com/${candidate.gitHub}/`}>
-                  <img
-                    className="h-5 w-5 inline-block"
-                    src="/images/gitHub.png"
-                    alt=""
-                  />
-                </a>
-              )}
+                </span>
+                <span className="tracking-wide capitalize">
+                  {candidate?.name}
+                </span>
+
+                {candidate.linkedInProfile && (
+                  <a
+                    href={`https://www.linkedin.com/in/${candidate.linkedInProfile}/`}
+                  >
+                    <img
+                      className="h-4 mb-1 w-4 inline-block"
+                      src="/images/linkendin.png"
+                      alt=""
+                    />
+                  </a>
+                )}
+                {candidate.gitHub && (
+                  <a href={`https://github.com/${candidate.gitHub}/`}>
+                    <img
+                      className="h-4 w-4 mb-1 inline-block"
+                      src="/images/github.png"
+                      alt=""
+                    />
+                  </a>
+                )}
+              </div>
+              <div className="flex justify-end">
+                <span onClick={handleEdit} className="cursor-pointer">
+                  <i className="fas fa-edit" color="action" />
+                </span>
+              </div>
             </div>
-            <div className="text-gray-700">
+            <div className="text-gray-700 py-4 px-1">
               <div className="grid md:grid-cols-2 text-sm">
                 <div className="grid grid-cols-2">
-                  <div className="px-4 py-2 font-semibold">Email</div>
-                  <div className="px-4 py-2">
+                  <div className="px-4 py-1 font-semibold">Email</div>
+                  <div className="px-4 py-1">
                     <a
                       className="text-blue-800"
                       href={`mailto:${candidate?.email}`}
@@ -166,25 +182,25 @@ const Find = () => {
                   </div>
                 </div>
                 <div className="grid grid-cols-2">
-                  <div className="px-4 py-2 font-semibold">Phone</div>
-                  <div className="px-4 py-2">{candidate?.contact}</div>
+                  <div className="px-4 py-1 font-semibold">Phone</div>
+                  <div className="px-4 py-1">{candidate?.contact}</div>
                 </div>
                 <div className="grid grid-cols-2">
-                  <div className="px-4 py-2 font-semibold">Current Salary</div>
-                  <div className="px-4 py-2">{candidate?.currentSalary}</div>
+                  <div className="px-4 py-1 font-semibold">Current Salary</div>
+                  <div className="px-4 py-1">{candidate?.currentSalary}</div>
                 </div>
                 <div className="grid grid-cols-2">
-                  <div className="px-4 py-2 font-semibold">Expected Salary</div>
-                  <div className="px-4 py-2">{candidate?.expectedSalary}</div>
+                  <div className="px-4 py-1 font-semibold">Expected Salary</div>
+                  <div className="px-4 py-1">{candidate?.expectedSalary}</div>
                 </div>
                 {candidate.offersInHand && (
                   <div className="grid grid-cols-2">
-                    <div className="px-4 py-2 font-semibold">
+                    <div className="px-4 py-1 font-semibold">
                       Offer Candidate have
                     </div>
                     <div>
                       {candidate.offersInHand.map((offers) => (
-                        <div className={'px-4 py-2 '} id={offers.company}>
+                        <div className={"px-4 py-1 "} id={offers.company}>
                           <img
                             className="h-5 w-5 inline-block"
                             src={`/images/${
@@ -194,7 +210,7 @@ const Find = () => {
                           />
                           <span className="font-semibold">
                             {offers.company}
-                          </span>{' '}
+                          </span>{" "}
                           of <span className="py-2">{offers.offer}</span>
                         </div>
                       ))}
@@ -202,15 +218,57 @@ const Find = () => {
                   </div>
                 )}
                 <div className="grid grid-cols-2">
-                  <div className="px-4 py-2 font-semibold">
+                  <div className="px-4 py-1 font-semibold">
                     Last updated time
                   </div>
-                  <div className="px-4 py-2">
+                  <div className="px-4 py-1">
+                    {new Date(candidate?.date).toGMTString().substring(0, 16)}
+                  </div>
+                </div>
+
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-1 font-semibold">YOE</div>
+                  <div className="px-4 py-1">5+ Year of experience</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-1 font-semibold">Skills</div>
+                  <div className="px-4 py-1 flex flex-wrap ">
+                    {/* <p> */}
+                    {skills.map((skill) => (
+                      <span class="list-none text-white  rounded-lg bg-indigo-700 border p-1">
+                        {skill}
+                      </span>
+                    ))}
+                    {/* </p> */}
+                  </div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-1 font-semibold">
+                    Vetting Rejected
+                  </div>
+                  <div className="px-4 py-1">
                     {new Date(candidate?.date).toGMTString().substring(0, 16)}
                   </div>
                 </div>
               </div>
             </div>
+            {/* <div className={"flex justify-between my-4 px-4"}>
+              <span className={"mx-1"}>
+                <button className="bg-blue-200 font-bold rounded-md px-3 py-1 text-black  hover:bg-blue-300 active:bg-blue-300 focus:outline-none focus:ring focus:ring-blue-300">
+                  Skill
+                </button>
+              </span>
+              <span className={"mx-1"}>
+                <button className="bg-red-200 font-bold rounded-md px-3 py-1 text-black  hover:bg-red-300 active:bg-red-300 focus:outline-none focus:ring focus:ring-red-300">
+                  Vetting results
+                </button>
+              </span>
+              <span className={"mx-1"}>
+                <button className="bg-green-200 font-bold rounded-md px-3 py-1 text-black  hover:bg-green-300 active:bg-green-300 focus:outline-none focus:ring focus:ring-green-300">
+                  Years of experience
+                </button>
+              </span>
+            </div> */}
           </div>
           <SkillView data={data} />
         </div>
